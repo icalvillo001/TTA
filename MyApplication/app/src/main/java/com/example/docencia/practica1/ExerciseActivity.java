4d94a33d91e5cce0ae57f42d500d07cfcee9be6a
icalvillo001 <icalvillo001@ehu.es>
2016-12-28 23:29:49 +0100

    conalgunas cosas de la parte 2 de la practica 2

--- MyApplication/app/src/main/java/com/example/docencia/practica1/ExerciseActivity.java
+++ MyApplication/app/src/main/java/com/example/docencia/practica1/ExerciseActivity.java
@@ -1,9 +1,17 @@
 package com.example.docencia.practica1;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by root on 23/12/16.
@@ -16,5 +24,51 @@ public class ExerciseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_exercise);
     }
 
+  /*  public void sendPicture(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null){
+                File dir= Environment.getExternalStoragePublicDirectory(Environment.);
+                try{
+                    File file=File.createTempFile("tta",".jpg",dir);
+                    pictureUri = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,pictureUri);
+                    startActivityForResult(intent, PICTURE_REQUEST_CODE);
+
+                }catch (IOException e){
+
+                }
+            }else{
+                Toast.makeText(this,R.string.no_app,Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+    public void recordVideo(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivityForResult(intent,VIDEO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.no_app,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void recordAudio(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+            Toast.makeText(this,R.string.no_micro,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivityForResult(intent,AUDIO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.no_app,Toast.LENGTH_SHORT).show();
+        }
+
+    }*/
 
 }
